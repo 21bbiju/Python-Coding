e9501408d081b6aa86151cedaf4aa26e0725b8ab
= <=>
2025-03-03 20:14:02 +0000

    Day 2 over

---
+++ Day02_homework.py
@@ -0,0 +1,6 @@
+my_birthday = "4th November 2008"
+sister_birthday = "3rd march 2002"
+father_birthday = "5th April 1987"
+mother_birthday = "20 June 1999"
+dog_birthday = "20 April 2023"
+print(f"my birthday is on {my_birthday}, my sister's birthday is on {sister_birthday} father's birthday is on {father_birthday},my mother's birthday is on {mother_birthday} and my dog's birthday is on {dog_birthday}.")
---
+++ Day02_variables.py
@@ -0,0 +1,9 @@
+name = "Basil"
+age = "16"
+country = "ireland"
+
+# print("hi my name is", name)
+# print("and my age is", age)
+# print("im from", country)
+
+print(f"my name is {name} and my age is {age}\n and i am from {country}")
