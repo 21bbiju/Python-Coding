2c06009ef8930269ca65c92c0d5682f1fb3558dd
= <=>
2025-03-10 21:07:26 +0000

    I did length of string,Indexing,Slicing and concatenantion.

---
+++ Day03_homework.py
@@ -0,0 +1,14 @@
+name = "Ireland"
+number_charters= len(name)
+print(number_charters)
+
+name2 = "Basil"
+print(name2 [0:3:1])
+
+name3 = "hi Basil"
+name4 = "how are you"
+print(name3 + name4)
+
+name5= "India"
+print(name5.upper())
+print(name5.lower())
