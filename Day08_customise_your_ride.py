5f2766dade9acdbda639ca1a4eb4b9ef23f6b266
= <=>
2025-04-30 19:30:04 +0100

    learned to customise ride

--- Day08_customise_your_ride.py
+++ Day08_customise_your_ride.py
@@ -1,22 +1,22 @@
 print("Select your ride: ")
 print("1. Bike")
 print("2. Car")
-choice = int(input("enter your choice"))
+choice = (input("enter your choice: "))
 
-if( choices==1 ):
+if( choice==1 ):
     print("1.Road Bike\n")
     print("2. Mountain Bike\n")
-    choice2 = int(input("enter your choice"))
-    if choice2==1:
+    choice2 = (input("enter your choice"))
+    if choice2== 1:
         print("you selected Road Bike")
     else:
         print("You have selected Mountain Bike")
-elif( choice==2 ):
+elif( choice== 2 ):
     print("what type of car")
     print("1.Nissan")
     print("2.Volkswagon")
-    choice3 = int(input("enter your choice"))
-    if choice3==1:
+    choice3 = (input("enter your choice"))
+    if choice3== 1:
         print("you have selected Nissan")
     else:
         print("you have selected Volkswagon")
