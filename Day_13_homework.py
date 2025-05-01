52c9990d36304d338d790839afda41fe7de2cd99
= <=>
2025-05-01 20:10:33 +0100

    Leanred how to check age and if student is able to enrol or not

---
+++ Day_13_homework.py
@@ -0,0 +1,7 @@
+number=input("enter your age : ")
+
+if number <="20-10":
+    print("you are able to enroll into this 10th grade class")
+
+else:
+    print("Im sorry but you are either to young or to old to enrol in this 10th grade class")
