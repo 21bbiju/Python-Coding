14e6a1cb8c37711aee7d4db8b5d08c09879ca860
= <=>
2025-06-24 20:17:21 +0100

    Learned to calculate the values of cos,sin and tan

---
+++ Day_18_project.py
@@ -0,0 +1,10 @@
+import math
+n = int(input("enter a number please: "))
+ans1 = math.cos(math.radians(n)) 
+print("the value of cos(n) is: ", ans1)
+
+ans2 = math.sin(math.radians(n)) 
+print("the value of sin(n) is: ",ans2)
+
+ans3 = math.tan(math.radians(n)) 
+print("the value of tan(n) is: ", ans3)
