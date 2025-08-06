3edbc0c5b5f406db84a33bbd256a7bd49f7ca3d1
= <=>
2025-08-06 19:38:32 +0100

    Figuered why there was an attribute error

---
+++ Day_29_private_attribute_example.py
@@ -0,0 +1,10 @@
+class Student:
+    __schoolName = 'XYZ school'
+        
+    def __init__(self, name, age):
+    
+        def __display(self):
+            print('this is private method.')
+
+std = Student("Bill", 25)
+print(std.__schoolName)
